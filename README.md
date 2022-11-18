dd99d76edadf7cedefeb96ecbcbf8f590fd6cb97
toemro <tomer0@rapyd.net>
2022-11-19 01:29:44 +0200

    a

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # goodies
+each goodies need to be:
+* fully commented so that every one who read it can under stand
+* a one line comment for one line of code max, for complexe comment add a url to an explenation page
---
+++ bash_goodies/bash commands.sh
@@ -0,0 +1,2 @@
+#!/bin/sh                                           #: add this comment in the biginnig of every script so both bash/sh can run it (if it not containig specific commands of one of them)*/
+bash -c "export PS1='\e[0;32m\u@\h:\W$\e[m'; bash"  #:makes bash print in color    
